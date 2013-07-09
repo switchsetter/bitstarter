da9d31c866a86eae5af165c101b05e7dcde92c52
switchsetter <switchsetter@outlook.com>
2013-07-09 23:30:55 +0000

    removed reading from file

--- web.js
+++ web.js
@@ -1,16 +1,16 @@
-var  fs = require('fs'); 
+//var  fs = require('fs'); 
 
    express = require('express');
 
 var app = express.createServer(express.logger());
 
 
-   var buffer = new Buffer(fs.readFileSync("index.html"),"utf-8");
+  // var buffer = new Buffer(fs.readFileSync("index.html"),"utf-8");
 
 
 app.get('/', function(request, response) {
-  // response.send('Hello World 2! I think there are better ways!');
-  response.send(buffer.toString("utf-8"));
+  response.send('Hello World 2! I think there are better ways!');
+ // response.send(buffer.toString("utf-8"));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
