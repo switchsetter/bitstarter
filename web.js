97de994ed720b8da48b69ded26cc6710ee57a55c
switchsetter <switchsetter@outlook.com>
2013-07-09 22:21:18 +0000

    added index.html

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2! I think there are better ways!');
+  // response.send('Hello World 2! I think there are better ways!');
+  response.send(fs.readFileSync("index.html"));
+
 });
 
 var port = process.env.PORT || 5000;
