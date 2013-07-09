9a2c5a93d9e84b9e12c95cdd2a3398d96ea0e0b6
switchsetter <switchsetter@outlook.com>
2013-07-09 23:52:16 +0000

    using buffer

--- web.js
+++ web.js
@@ -4,12 +4,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer(fs.readFileSync("index.html"),"utf-8");
+
+
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync("index.html"));
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
