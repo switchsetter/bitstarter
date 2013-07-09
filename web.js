d2421ecff1186f800f4ebfa535e732038a9bb358
switchsetter <switchsetter@outlook.com>
2013-07-09 23:45:48 +0000

    retyped js

--- web.js
+++ web.js
@@ -1,18 +1,15 @@
-//var  fs = require('fs'); 
+var  fs = require('fs');
 
-   express = require('express');
+var express = require('express');
 
 var app = express.createServer(express.logger());
 
-
-  // var buffer = new Buffer(fs.readFileSync("index.html"),"utf-8");
-
-
 app.get('/', function(request, response) {
-  response.send('Hello World 2! I think there are better ways!');
- // response.send(buffer.toString("utf-8"));
+  response.send(fs.readFileSync("index.html"));
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
