540987ecf1029977279410398489011868295187
switchsetter <switchsetter@outlook.com>
2013-07-19 13:00:23 +0000

    changed basic wireframe to Responsive Design and CSS Styling

--- grader.js
+++ grader.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 /*
-Automatically grade files for the presence of specified HTML tags/attributes.
+Automatically grade files/web pages for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
 and basic DOM parsing.
 */
@@ -13,6 +13,8 @@ var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
+var checks;
+
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
@@ -20,6 +22,12 @@ var assertFileExists = function(infile) {
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+};
+
+
+
+var cheerioHtml = function(html) {
+    return cheerio.load(html);
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -41,20 +49,63 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+
+var checkHtml = function(html, checksfile) {
+    $ = cheerioHtml(html);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
+
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
+
+
+var getResult = function(result, response) {
+    if (result instanceof Error) {
+	console.log('Error: ' + result.message);
+	return;
+	}
+   //console.log('result='+result);
+    var checkJson = checkHtml(result, checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+};
+
+
+
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Url of page')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+   
+    if( program.url ){
+        checks = program.checks;
+        rest.get(program.url).on('complete',getResult);
+    }
+    else if(program.file){
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
+    else{
+        console.log('please enter -f file or -u url, for help -help');
+    }
+    
+    
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
